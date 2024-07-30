a2c2564001ff1bcd1e5a7fccf3e2838edbd45549
gourab8389 <deyg6988@gmail.com>
2024-07-30 19:13:40 +0530

    Project setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Poppins} from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight:["100","200","300","400","500","600","700","800","900"] });
+
+export const metadata: Metadata = {
+  title: "Job Portal",
+  description: "Create your own onlinw job applications",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
