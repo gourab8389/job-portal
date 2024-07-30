2c64b77a3fd14e36b596f0f44d08eb2249a90599
gourab8389 <deyg6988@gmail.com>
2024-07-30 22:04:25 +0530

    chroes: navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Navbar from './_components/navbar'
 
 const DashboardLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className='h-full'>
       {/** navbar */}
         <header className='h-20 fixed inset-y-0 w-full z-50'>
-            header
+            <Navbar/>
         </header>
 
       {/** sidebar */}
