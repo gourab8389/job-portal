8bdd56d332a87099d3fb0a507a82a25fb832e4da
gourab8389 <deyg6988@gmail.com>
2024-07-30 20:16:38 +0530

    auth add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Poppins} from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({ subsets: ["latin"], weight:["100","200","300","400","500","600","700","800","900"] });
 
 export const metadata: Metadata = {
-  title: "Job Portal",
+  title: "jobQuest",
   description: "Create your own onlinw job applications",
 };
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
