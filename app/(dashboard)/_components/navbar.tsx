4d7ad9eaaeefef5e1ca14f09040e6c0d6d8fe574
gourab8389 <deyg6988@gmail.com>
2024-07-31 18:46:01 +0530

    mobile nav

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import NavbarRoutes from './navbar-routes'
+import MobileSideBar from './mobile-side-bar'
 
 const Navbar = () => {
   return (
     <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
-        {/* <MobileSideBar/> */}
+        <MobileSideBar/>
       <NavbarRoutes/>
     </div>
   )
--- app/(dashboard)/_components/side-bar-route-item.tsx
+++ app/(dashboard)/_components/side-bar-route-item.tsx
@@ -22,11 +22,12 @@ const SideBarRouteItem = ({
         router.push(href);
     }
   return (
-    <Button variant={'outline'} onClick={onClick} className={cn("flex gap-x-2 text-neutral-500 text-sm font-[500] pl-2 transition-all hover:text-neutral-600 hover:bg-neutral-300/20 border-none my-1", isActive && "text-purple-700 bg-purple-700/20 hover:bg-purple-700/20 hover:text-purple-700")}>
-        <div className="flex gap-x-2 py-4 bg-red-500">
-            <Icon size={22} />
+    <Button variant={'outline'} onClick={onClick} className={cn("flex items-center gap-x-2 text-neutral-500 text-sm font-[500] pl-6 transition-all hover:text-neutral-600 hover:bg-neutral-300/20 border-none my-1", isActive && "text-purple-700 bg-purple-200/20 hover:bg-purple-700/20 hover:text-purple-700")}>
+        <div className="flex items-center gap-x-2 py-4">
+            <Icon className={cn("text-neutral-500", isActive && "text-purple-700")} size={22} />
             {label}
         </div>
+        <div className={cn("ml-auto opacity-0 border-2 border-purple-700 h-full transition-all", isActive && "opacity-100")}></div>
     </Button>
   )
 }
