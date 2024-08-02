acfd0d475877bc3ba1cd7096998e685bfeb2b46b
gourab8389 <deyg6988@gmail.com>
2024-08-02 23:30:50 +0530

    add api for job id

---
+++ app/api/jobs/[jobId]/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export const PATCH = async (req: Request, {params}: {params:{jobId:string}}) => {
+    try {
+
+        const {userId} = auth();
+        const {jobId} = params;
+
+        const {title} = await req.json();
+
+        if(!userId){
+            return new NextResponse("Un-Authorized", { status: 401} );
+        }
+        if(!jobId){
+            return new NextResponse("Id is missing", { status: 401} );
+        }
+        
+        const job = await db.job.update({
+            where:{
+                id: jobId,
+                userId,
+            },
+            data : {title},
+        })
+
+        return NextResponse.json(job);
+
+    } catch (error) {
+        console.log(`[JOB_PATCH] : ${error}`);
+        return new NextResponse("Internal Server Error", {status: 500});
+    }
+}
