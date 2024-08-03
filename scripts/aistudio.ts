b60234270cc6bb67f129d2f731eee785ff99f5a1
gourab8389 <deyg6988@gmail.com>
2024-08-04 03:43:18 +0530

    update aistudio

--- scripts/aistudio.ts
+++ scripts/aistudio.ts
@@ -14,7 +14,7 @@ const {
   } = require("@google/generative-ai");
   
   // const apiKey = process.env.GEMINI_API_KEY;
-  const genAI = new GoogleGenerativeAI();
+  const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
   
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
