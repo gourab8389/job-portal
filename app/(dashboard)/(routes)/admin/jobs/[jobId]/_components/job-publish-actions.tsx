e3eb9ccfd24176ab242c2040451a89f32509d846
gourab8389 <deyg6988@gmail.com>
2024-08-05 13:08:43 +0530

    refactor: publish and delete

--- app/(dashboard)/(routes)/admin/jobs/[jobId]/_components/job-publish-actions.tsx
+++ app/(dashboard)/(routes)/admin/jobs/[jobId]/_components/job-publish-actions.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface JobPublishActionProps {
     disabled: boolean;
@@ -14,12 +17,42 @@ const JobPublishAction = ({ disabled, jobId, isPublished }: JobPublishActionProp
 
 
     const [isLoading, setIsLoading] = useState(false)
+    const router = useRouter()
 
-    const onClick = () => {
-
+    const onClick = async () => {
+        try {
+            setIsLoading(true)
+            if(isPublished){
+                await axios.patch(`/api/jobs/${jobId}/unpublish`);
+                toast.success("Job Un-Published")
+            }else{
+                await axios.patch(`/api/jobs/${jobId}/publish`);
+                toast.success("Job Published")
+            }
+            router.refresh()
+        } catch (error) {
+            toast.error("Something went wrong")
+            console.log((error as Error)?.message)
+        }finally{
+            setIsLoading(false)
+        }
     }
-    const onDelete = () => {
 
+
+    const onDelete = async () => {
+        try {
+            setIsLoading(true)
+            
+            await axios.delete(`/api/jobs/${jobId}`)
+            toast.success("Job deleted successfully")
+            router.refresh();
+            router.push("/admin/jobs")
+        } catch (error) {
+            toast.error("Something went wrong")
+            console.log((error as Error)?.message)
+        }finally{
+            setIsLoading(false)
+        }  
     }
 
     return (
--- app/api/jobs/[jobId]/route.ts
+++ app/api/jobs/[jobId]/route.ts
@@ -1,5 +1,7 @@
+import { storage } from "@/config/firebase.config";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { deleteObject, ref } from "firebase/storage";
 import { NextResponse } from "next/server";
 
 export const PATCH = async (req: Request, { params }: { params: { jobId: string } }) => {
@@ -31,3 +33,50 @@ export const PATCH = async (req: Request, { params }: { params: { jobId: string 
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 };
+
+
+
+export const DELETE = async (req: Request, { params }: { params: { jobId: string } }) => {
+  try {
+    const { userId } = auth();
+    const { jobId } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!jobId) {
+      return new NextResponse("Id is missing", { status: 401 });
+    }
+
+    const job = await db.job.findUnique({
+      where : {
+        id: jobId,
+        userId
+      },
+    })
+
+    if (!jobId) {
+      return new NextResponse("Job not found", { status: 404 });
+    }
+
+    if(job?.imageUrl){
+
+      const storageRef = ref(storage, job.imageUrl)
+      await deleteObject(storageRef)
+    }
+
+    const deleteJob = await db.job.delete({
+      where : {
+        id: jobId,
+        userId
+      },
+    })
+
+    return NextResponse.json(deleteJob)
+
+  } catch (error) {
+    console.log(`[JOB_DELETE] : ${error}`);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+};
+
