1fb1a33129b1b17990ce3b5b9f23da715624ddd3
gourab8389 <deyg6988@gmail.com>
2024-08-07 19:01:16 +0530

    refactor: date-filter, add select for date & get-job update

--- actions/get-job.ts
+++ actions/get-job.ts
@@ -52,6 +52,42 @@ export const getJobs = async ({
             }
         }
 
+        if(createdAtFilter){
+            const currentDate = new Date();
+            let startDate : Date;
+            switch(createdAtFilter){
+                case "today" :
+                    startDate = new Date(currentDate);
+                    break;
+
+                case "yesterday" :
+                    startDate = new Date(currentDate);
+                    startDate.setDate(startDate.getDate()-1)
+                    break;
+
+                case "thisWeek" :
+                    startDate = new Date(currentDate);
+                    startDate.setDate(startDate.getDate() - currentDate.getDay())
+                    break;
+                
+                case "lastWeek" :
+                    startDate = new Date(currentDate);
+                    startDate.setDate(startDate.getDate() - currentDate.getDay() - 7)
+                    break;
+
+                case "thisMonth" :
+                    startDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+                    break;
+                
+                default :
+                startDate = new Date(0);    
+            }
+
+            query.where.createdAt = {
+                gte : startDate
+            }
+        }
+
         const jobs = await db.job.findMany(query)
         return jobs
 
