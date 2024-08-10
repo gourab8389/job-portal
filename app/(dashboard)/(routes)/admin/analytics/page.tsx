f44f22ae43af6c5c5e1fbf2a45f6b1b125144fe4
gourab8389 <deyg6988@gmail.com>
2024-08-10 23:56:25 +0530

    chroes: analytics page

---
+++ app/(dashboard)/(routes)/admin/analytics/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div className='items-center justify-center p-5'>
+      <h1>This is Analytics page</h1>
+      <p>for now it's not needed</p>
+    </div>
+  )
+}
+
+export default Analytics
